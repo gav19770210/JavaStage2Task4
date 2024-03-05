dba7d665e7599b4675ec7bfb18b8c4b7c7e04f6e
gav19770210 <gav19770210@yandex.ru>
2024-03-05 15:34:13 +0700

    first commit

--- src/main/java/ru/gav19770210/stage2task4/db/LogDbWriterImp.java
+++ src/main/java/ru/gav19770210/stage2task4/db/LogDbWriterImp.java
@@ -4,7 +4,6 @@ import jakarta.persistence.NoResultException;
 import lombok.Getter;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.gav19770210.stage2task4.model.LogRow;
